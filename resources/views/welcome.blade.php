e58b2480284ece2451e94f521124a6d566f86655
Robin KAWKA <rkawka@orange.fr>
2023-12-10 23:41:20 +0100

    remove bootstrap and composition api for article

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,6 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-        <!-- <link href="https://getbootstrap.com/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet"> -->
         @vite(['resources/js/app.js', 'resources/css/app.css'])
 
 
