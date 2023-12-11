09760766c6ad456d6c12ed9eefb9a29b105db64f
Robin KAWKA <rkawka@orange.fr>
2023-12-11 21:51:30 +0100

    change numbers of created article in seed to 10

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -13,6 +13,6 @@ class ArticleSeeder extends Seeder
      */
     public function run(): void
     {
-        Article::factory()->count(3)->create();
+        Article::factory()->count(10)->create();
     }
 }
